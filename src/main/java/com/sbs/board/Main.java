2ce271e955ba4f725545764447ae5a5c67824d74
SangWon7242 <gotkdqja@gmail.com>
2025-07-20 11:35:06 +0900

    게시물 기본 명령어 처리

--- src/main/java/com/sbs/board/Main.java
+++ src/main/java/com/sbs/board/Main.java
@@ -1,7 +1,11 @@
 package com.sbs.board;
 
+import com.sbs.board.bundedContext.app.App;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("Hello and welcome!");
+    // 프로그램의 시작점
+    App app = new App();
+    app.run();
   }
 }
---
+++ src/main/java/com/sbs/board/bundedContext/app/App.java
@@ -0,0 +1,43 @@
+package com.sbs.board.bundedContext.app;
+
+import com.sbs.board.bundedContext.container.Container;
+
+import java.util.Scanner;
+
+public class App {
+  public Scanner sc;
+
+  public App() {
+    sc = Container.sc;
+  }
+
+  public void run() {
+    System.out.println("== 게시판 프로그램 시작 ==");
+
+    while (true) {
+      System.out.print("명령어) ");
+      String cmd = sc.nextLine().trim();
+
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시글 작성 ==");
+
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String subject = sc.nextLine();
+
+        int id = 1;
+        System.out.printf("%d번 게시물이 작성되었습니다.\n", id);
+      } else if (cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      } else {
+        System.out.println("알 수 없는 명령어입니다.");
+      }
+    }
+
+    System.out.println("== 게시판 프로그램 끝 =");
+    sc.close();
+  }
+}
